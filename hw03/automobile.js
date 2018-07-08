c670d9c317907175839db46cd88da4f5871bbe8c
Alexander Goodman <goodmaal@flip2.engr.oregonstate.edu>
2018-07-08 13:52:46 -0700

    updated all functions and results are displayed

--- hw03/automobile.js
+++ hw03/automobile.js
@@ -5,7 +5,7 @@
  
  * REFERENCES: 
  * 	1. OSU CS 290 Week 03 Lectures
- *  2. 
+ *  	2. Eloquent JavaScript Third Edition by Marijn Haverbeke 
 */
 
 function Automobile( year, make, model, type ){
@@ -26,7 +26,22 @@ var automobiles = [
 
 /*This function sorts arrays using an arbitrary comparator. You pass it a comparator and an array of objects appropriate for that comparator and it will return a new array which is sorted with the largest object in index 0 and the smallest in the last index*/
 function sortArr( comparator, array ){
-    /*your code here*/
+  var arrSorted = array.slice();	// array to hold the sorted objects (found slice on w3schools.com)   
+  var length = array.length;		// Number of Elements in the array
+  var temp;				// placeholder while swapping elements
+  // Since we are dealing with a small number of objects, the simple Bubble Sort algorithm will be used
+  // 	If the automobiles array held a large number of Automobile objects, then merge sort would be implemented
+  for(var i=0; i<length-1; i++){
+    for(var j=i+1; j<length; j++){
+      // if arrSorted[i] < arrSorted[j] then swap
+      if(comparator(arrSorted[j], arrSorted[i])){
+	temp = arrSorted[i];
+	arrSorted[i] = arrSorted[j];
+	arrSorted[j] = temp;
+      }
+    }
+  }
+  return arrSorted;
 }
 
 /*A comparator takes two arguments and uses some algorithm to compare them. If the first argument is larger or greater than the 2nd it returns true, otherwise it returns false. Here is an example that works on integers*/
@@ -42,23 +57,106 @@ function exComparator( int1, int2){
 
 /*This compares two automobiles based on their year. Newer cars are "greater" than older cars.*/
 function yearComparator( auto1, auto2){
-    /* your code here*/
+  if(auto1.year > auto2.year){
+	return true;
+  }
+  else{
+	return false;
+  }    
+
 }
 
 /*This compares two automobiles based on their make. It should be case insensitive and makes which are alphabetically earlier in the alphabet are "greater" than ones that come later.*/
 function makeComparator( auto1, auto2){
-    /* your code here*/
+   // Making this case insensitive
+  if(auto1.make.toUpperCase() < auto2.make.toUpperCase()){
+	return true;
+  }
+  else{
+	return false;
+  }
 }
 
 /*This compares two automobiles based on their type. The ordering from "greatest" to "least" is as follows: roadster, pickup, suv, wagon, (types not otherwise listed). It should be case insensitive. If two cars are of equal type then the newest one by model year should be considered "greater".*/
 function typeComparator( auto1, auto2){
-    /* your code here*/
+
+  var typeVal = function(vehicle){
+    // Reference: Chapter 2 Eloquent JavaScript Third Edition - Marijn Haverbeke
+    switch(vehicle.type.toUpperCase()){
+	case "ROADSTER":
+	  return 1;
+	  break;
+	case "PICKUP":
+	  return 2;
+	  break;
+	case "SUV":
+	  return 3;
+	  break;
+	case "WAGON":
+	  return 4;
+	  break;
+	default:
+	  return 5;
+	  break;
+    }
+  }
+
+  // Handles cases with same Types
+  if(typeVal(auto1) === typeVal(auto2)){
+    if(auto1.year > auto2.year){
+	return true;
+    }
+    else{
+	return false;
+    }
+  }
+  else if(typeVal(auto1) < typeVal(auto2)){
+	return true;
+  }
+  else{
+	return false;
+  }
+
+
 }
 
 /*Your program should output the following to the console.log, including the opening and closing 5 stars. All values in parenthesis should be replaced with appropriate values. Each line is a seperate call to console.log.
 
 Each line representing a car should be produced via a logMe function. This function should be added to the Automobile class and accept a single boolean argument. If the argument is 'true' then it prints "year make model type" with the year, make, model and type being the values appropriate for the automobile. If the argument is 'false' then the type is ommited and just the "year make model" is logged.
+*/
 
+Automobile.prototype.logMe = function(x){
+  if(x){
+    console.log(this.year + " " + this.make + " " + this.model + " " + this.type);
+  }
+  else{
+    console.log(this.year + " " + this.make + " " + this.model);
+  }
+
+};
+
+//Prints the objects to the console.
+function display(arr, bool){
+  arr.forEach(function(x){
+    x.logMe(bool);
+  })
+}
+
+//display(automobiles, true);
+
+var arrYear = sortArr(yearComparator, automobiles);
+var arrMake = sortArr(makeComparator, automobiles);
+var arrType = sortArr(typeComparator, automobiles);
+
+console.log("*****");
+console.log("The cars sorted by year are: ");
+display(arrYear, false);
+console.log("\nThe cars sorted by make are: ");
+display(arrMake, false);
+console.log("\nThe cars sorted by type are: ");
+display(arrType, true);
+console.log("*****");
+/*
 *****
 The cars sorted by year are:
 (year make model of the 'greatest' car)
